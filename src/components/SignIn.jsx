37c04fb4aee7123801543226cca1efd4d3ab74f2
Nurudeenbika <nurudeenbika@gmail.com>
2025-08-02 15:25:28 +0100

    feat: added 10.9 solution

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,33 +1,65 @@
 import React from "react";
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 import { Formik } from "formik";
+import * as yup from "yup";
 
-// This is the function that will be called when the form is submitted.
-// For this exercise, we'll just log the values.
+// Define a validation schema using Yup
+const validationSchema = yup.object().shape({
+  username: yup.string().required("Username is required"),
+  password: yup.string().required("Password is required"),
+});
+
 const onSubmit = (values) => {
   console.log(values);
 };
 
 const SignIn = () => {
   return (
-    <Formik initialValues={{ username: "", password: "" }} onSubmit={onSubmit}>
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+      }) => (
         <View style={styles.container}>
           <TextInput
-            style={styles.input}
+            style={[
+              styles.input,
+              touched.username && errors.username && styles.errorInput,
+            ]}
             placeholder="Username"
             onChangeText={handleChange("username")}
             onBlur={handleBlur("username")}
             value={values.username}
+            error={touched.username && errors.username}
           />
+          {/* Display error message */}
+          {touched.username && errors.username && (
+            <Text style={styles.errorText}>{errors.username}</Text>
+          )}
           <TextInput
-            style={styles.input}
+            style={[
+              styles.input,
+              touched.password && errors.password && styles.errorInput,
+            ]}
             placeholder="Password"
             secureTextEntry // This is the prop to obscure the password input.
             onChangeText={handleChange("password")}
             onBlur={handleBlur("password")}
             value={values.password}
+            error={touched.password && errors.password}
           />
+          {/* Display error message */}
+          {touched.password && errors.password && (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          )}
           <Button onPress={handleSubmit} title="Sign In" />
         </View>
       )}
@@ -43,8 +75,15 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
-    marginBottom: 10,
+    marginBottom: 5,
     paddingHorizontal: 8,
+  },
+  errorInput: {
+    borderColor: "#d73a4a",
+  },
+  errorText: {
+    color: "#d73a4a",
+    marginBottom: 10,
   },
 });
 
